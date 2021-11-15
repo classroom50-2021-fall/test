a8034ff52242b9a8c434cb725c9f2106914e3269
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2021-11-15 10:27:42 -0500

    add wheels distribution

---
+++ wheels/ctype.c
@@ -0,0 +1,32 @@
+#include "ctype.h"
+
+bool isalpha(char c)
+{
+    // Return true if c is alphabetical (uppercase or lowercase), else false
+    return false;
+}
+
+bool isdigit(char c)
+{
+    // Return true if c is a decimal digit, else false
+    return false;
+}
+
+bool isalnum(char c)
+{
+    // Return true if c is alphabetical (uppercase or lowercase)
+    // or numeric (i.e., a decimal digit), else false
+    return false;
+}
+
+bool isallupper(char *s)
+{
+    // Return true if every char in s is alphabetical and uppercase, else false
+    return false;
+}
+
+void toallupper(char *s)
+{
+    // Change any lowercase char in s to uppercase
+    return;
+}
---
+++ wheels/ctype.h
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+bool isalpha(char c);
+bool isdigit(char c);
+bool isalnum(char c);
+bool isallupper(char *s);
+void toallupper(char *s);
